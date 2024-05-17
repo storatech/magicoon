1fa709830fd0b572d0e9aeccdfdc0b8b97312b25
Batmend Ganbaatar <batmend@gege.mn>
2024-05-17 14:34:25 +0800

    aa

--- dist/styles.js
+++ dist/styles.js
@@ -1,2 +1,74 @@
-import { styles_default } from "./chunk-MRI4W76N.js";
+// magicoon_react/styles.ts
+function _define_property(obj, key, value) {
+    if (key in obj) {
+        Object.defineProperty(obj, key, {
+            value: value,
+            enumerable: true,
+            configurable: true,
+            writable: true
+        });
+    } else {
+        obj[key] = value;
+    }
+    return obj;
+}
+function _object_spread(target) {
+    for(var i = 1; i < arguments.length; i++){
+        var source = arguments[i] != null ? arguments[i] : {};
+        var ownKeys = Object.keys(source);
+        if (typeof Object.getOwnPropertySymbols === "function") {
+            ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function(sym) {
+                return Object.getOwnPropertyDescriptor(source, sym).enumerable;
+            }));
+        }
+        ownKeys.forEach(function(key) {
+            _define_property(target, key, source[key]);
+        });
+    }
+    return target;
+}
+function ownKeys(object, enumerableOnly) {
+    var keys = Object.keys(object);
+    if (Object.getOwnPropertySymbols) {
+        var symbols = Object.getOwnPropertySymbols(object);
+        if (enumerableOnly) {
+            symbols = symbols.filter(function(sym) {
+                return Object.getOwnPropertyDescriptor(object, sym).enumerable;
+            });
+        }
+        keys.push.apply(keys, symbols);
+    }
+    return keys;
+}
+function _object_spread_props(target, source) {
+    source = source != null ? source : {};
+    if (Object.getOwnPropertyDescriptors) {
+        Object.defineProperties(target, Object.getOwnPropertyDescriptors(source));
+    } else {
+        ownKeys(Object(source)).forEach(function(key) {
+            Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key));
+        });
+    }
+    return target;
+}
+var defaultStyle = {
+    fontFamily: "Magicoon Regular",
+    fontSize: "24px",
+    display: "inline-block",
+    verticalAlign: "middle",
+    lineHeight: "1",
+    whiteSpace: "nowrap"
+};
+var filledStyle = _object_spread_props(_object_spread({}, defaultStyle), {
+    fontFamily: "Magicoon Filled"
+});
+var lightStyle = _object_spread_props(_object_spread({}, defaultStyle), {
+    fontFamily: "Magicoon Light"
+});
+var styles = {
+    filled: filledStyle,
+    light: lightStyle,
+    regular: defaultStyle
+};
+var styles_default = styles;
 export { styles_default as default };
